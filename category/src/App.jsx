4a32e0e34a26dc004a2fe475121219d9626908d1
shivamsainis007 <shivamsainis138@gmail.com>
2025-06-23 17:01:14 +0530

    category code file

---
+++ category/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Category from "./components/Category";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Category />
+    </div>
+  );
+}
+
+export default App;
